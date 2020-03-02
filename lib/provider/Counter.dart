663708aa6d5497b5d86d93d254656d86aa7496fd
enotsss <enochwesss@gmail.com>
2020-03-02 15:42:17 +0800

    Flutter官方推荐的状态管理库 provider 的使用

--- lib/provider/Counter.dart
+++ lib/provider/Counter.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
 
 // 创建方法继承 ChangeNotifier
-class Counter with ChangeNotifier{
-
+class Counter with ChangeNotifier {
   // 创建状态 在不同组件之间实现共享
   int _count = 0;
+
+  // 若初始化就希望能赋值 可以放入构造函数里面
+  // 在构造函数内 可以获取本地存储的值进行赋值
+  Counter() {
+    this._count = 10;
+  }
 
   // 获取状态 让外部可以拿到 私有方法 _count
   int get count => _count;
 
   // 更新状态
-  incCount(){
-
+  incCount() {
     this._count++;
     // 更新状态
     notifyListeners();
   }
-
 }
